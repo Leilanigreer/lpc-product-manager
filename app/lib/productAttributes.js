023531cbdd3117af50b4c45cf9415206cc61baac
Leilanigreer <leilani@lpcgolf.com>
2024-10-23 14:45:32 -0700

    added Variant Name Logic and ProductType. Also started updating the GraphQL with variables

--- app/lib/productAttributes.js
+++ app/lib/productAttributes.js
@@ -212,12 +212,12 @@ export const generateTitle = (formState, leatherColors, threadColors) => {
   return title;
 };
 
-export const generateHandle = (formState, title) => {
+export const generateMainHandle = (formState, title) => {
   if (!title || title === "Pending Title") {
-    return "pending-handle";
+    return "pending-main-handle";
   }
 
-  const tempHandle =title.toLowerCase()
+  const tempMainHandle =title.toLowerCase()
                           .replace(/\s+/g, '-')  // Replace all spaces with hyphens
                           .replace(/[^a-z0-9-]/g, ''); // Remove any special characters
 
@@ -227,18 +227,156 @@ export const generateHandle = (formState, title) => {
     case COLLECTION_TYPES.QUILTED:
     case COLLECTION_TYPES.ANIMAL:
     case COLLECTION_TYPES.CLASSIC:
-      return `${tempHandle}-golf-headcovers`;
+      return `${tempMainHandle}-golf-headcovers`;
      
     case COLLECTION_TYPES.ARGYLE:
-      return `${tempHandle}-argyle-golf-headcovers`; 
+      return `${tempMainHandle}-argyle-golf-headcovers`; 
 
     case COLLECTION_TYPES.QCLASSIC:
-      return `${tempHandle}-quilted-golf-headcovers`;
+      return `${tempMainHandle}-quilted-golf-headcovers`;
     
       default:
-        return "pendling-handle";
+        return "pendling-main-handle";
   }
 };
+
+export const generateVariantNames = async (formState, shapes, styles) => {
+  console.log("generateVariantName called with formState:", JSON.stringify(formState, null, 2));
+    
+  if (!formState || !shapes ) {
+    console.error("Missing required parameters for generateVariantName");
+    console.log("formState:", formState);
+    return [];
+  }
+
+  const offeringType = (formState.selectedOfferingType)
+  const collectionType = getCollectionType(formState.selectedCollection);
+  const woodLabel = ['3-Wood', '5-Wood', '7-Wood'];
+  
+  // Check for styles only if the collection type requires it
+  if ((collectionType === COLLECTION_TYPES.ANIMAL || 
+    collectionType === COLLECTION_TYPES.CLASSIC || 
+    collectionType === COLLECTION_TYPES.QCLASSIC) && !styles) {
+    console.error("Missing styles for a collection type that requires it");
+  return [];
+  }
+
+  let variantNames = Object.entries(formState.weights || {})
+    .filter(([_, weight]) => weight !== "") // Only generate variantNames for shapes with non-empty weights
+    .map(([shapeId, weight]) => {
+      const shape = shapes.find(s => s.value === shapeId);
+      
+      if (!shape) {
+        console.error(`Shape not found for id: ${shapeId}`);
+        return null;
+      }
+
+      const selectedStyleId = formState.selectedStyles?.[shapeId];
+      const selectedStyle = styles?.find(style => style.value === selectedStyleId);
+
+
+      let variantNamesParts = [];
+
+      switch (collectionType) {
+        case COLLECTION_TYPES.QUILTED:
+        case COLLECTION_TYPES.ARGYLE:
+          variantNamesParts = [
+            shape.label
+          ];
+          break;
+        case COLLECTION_TYPES.ANIMAL:
+        case COLLECTION_TYPES.CLASSIC:
+        case COLLECTION_TYPES.QCLASSIC:
+          variantNamesParts = [
+            selectedStyle?.label,
+            shape.label
+          ];
+          break;
+        default:
+          return null;
+      }
+
+      return { 
+        variantName: variantNamesParts.join(' ').trim(), 
+        shape: shape.label,
+        style: selectedStyle?.label, 
+        shapeId 
+      };
+    })
+    .filter(item => item !== null);
+
+  console.log("Initial variantNames generated:", variantNames);
+
+  let finalVariantNames = [];
+
+  finalVariantNames = variantNames.map(item => item.variantName);
+
+
+  if (offeringType === "customizable") {
+    // const hasFairway = variantNames.some(item => item.shape === 'Fairway');
+    let customVariantNames = [];
+
+    switch(collectionType) {
+      case COLLECTION_TYPES.QUILTED:
+      case COLLECTION_TYPES.ARGYLE:
+      
+        let addedFairwayCustom = false;
+
+        variantNames.forEach(item => {
+          if (item.shape === 'Fairway' || woodLabel.includes(item.shape)) {
+            if (!addedFairwayCustom) {
+              customVariantNames.push(`Customize Fairway +$15`);
+              addedFairwayCustom = true;
+            }
+          } else {
+            customVariantNames.push(`Customize ${item.variantName} +$15`);
+          }
+      });
+      break;
+
+      case COLLECTION_TYPES.ANIMAL:
+      case COLLECTION_TYPES.CLASSIC:
+      case COLLECTION_TYPES.QCLASSIC:
+        variantNames.forEach(item => {
+          if (item.shape === 'Fairway' || woodLabel.includes(item.shape)) {
+            // Replace wood shape with Fairway but keep the style
+            const fairwayVariantName = item.variantName.replace(
+              new RegExp(`(${woodLabel.join('|')}|Fairway|-Wood)`), 
+              'Fairway'
+            );
+            customVariantNames.push(`Customize ${fairwayVariantName} +$15`);
+          } else {
+            customVariantNames.push(`Customize ${item.variantName} +$15`);
+          }
+        });
+        break;
+    }
+
+    // Combine regular and custom variant names
+    finalVariantNames = [...finalVariantNames, ...customVariantNames];
+  }
+
+  return finalVariantNames;
+};
+
+export const generateProductType = (formState) => {
+  const collectionType = getCollectionType(formState.selectedCollection);
+
+  if (collectionType === COLLECTION_TYPES.QUILTED) {
+    return "Quilted"
+  } else if (collectionType === COLLECTION_TYPES.ANIMAL) {
+    return "Animal Print"
+  } else if (collectionType === COLLECTION_TYPES.ARGYLE) {
+    return "Argyle"
+  } else if (collectionType === COLLECTION_TYPES.CLASSIC) {
+    return "Classic"
+  } else if (collectionType === COLLECTION_TYPES.QCLASSIC) {
+    return "QClassic"
+  } else {
+    return ""
+  }
+};
+
 
 // export const generateDescription = (formState, fonts) => {
 //   // const font = getFonts(formState, fonts);
