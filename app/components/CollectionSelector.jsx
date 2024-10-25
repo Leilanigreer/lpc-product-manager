b49e114c5af0a4163b9bee095f7d2efc90cdb38b
Leilanigreer <leilani@lpcgolf.com>
2024-10-24 18:09:45 -0700

    cleaned up productAttributes file and added the ability to connect and see the weight.

--- app/components/CollectionSelector.jsx
+++ app/components/CollectionSelector.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Select } from "@shopify/polaris";
 
 const CollectionSelector = ({ shopifyCollections, selectedCollection, onChange }) => {
-  // console.log(shopifyCollections);
-
-  const options = shopifyCollections ? shopifyCollections.map(collection => ({
-    label: collection.label || collection.title, // Use label if exists, fall back to title
-    value: collection.value || collection.id     // Use value if exists, fall back to id
-  })) : [];
-
-  // console.log('Collection Options:', options); // Debug log
+  const options = [    
+    { label: 'Select a collection...', value: '' }, 
+    ...(shopifyCollections ? shopifyCollections.map(collection => ({
+      label: collection.label || collection.title,
+      value: collection.value || collection.id
+    })) : [])
+  ];
 
   return (
     <Select
