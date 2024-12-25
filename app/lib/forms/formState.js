45eb1471bfaaf25004e2c68bbf05beeda626218e
Leilanigreer <leilani@lpcgolf.com>
2024-12-24 19:22:27 -0800

    migrated to a folder for organization

---
+++ app/lib/forms/formState.js
@@ -0,0 +1,18 @@
+export const initialFormState = {
+  selectedCollection: "",
+  selectedOfferingType: "",
+  limitedEditionQuantity: "",
+  selectedLeatherColor1: "",
+  selectedLeatherColor2: "",
+  selectedStitchingColor: "",
+  selectedEmbroideryColor: "",
+  selectedEmbroideryColors: {},
+  selectedFont: "",
+  selectedShapes: {},
+  selectedStyles: {},
+  weights: {},
+  qClassicLeathers: {},
+  matchingAmannNumber: "",
+  matchingIsacordNumber: "",
+  shapeIsacordNumbers: {},
+};
