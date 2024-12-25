3ba5d3f821f30766f15dab15b1fcdc5d60655def
Leilanigreer <leilani@lpcgolf.com>
2024-12-24 19:14:42 -0800

    updated location/name of loader and removed depreciated json

---
+++ app/lib/loaders/index.js
@@ -0,0 +1,88 @@
+// app/lib/loaders/index.js
+
+import { 
+  getLeatherColors, 
+  getStitchingThreadColors, 
+  getEmbroideryThreadColors, 
+  getColorTags, 
+  getFonts, 
+  getShapes, 
+  getStyles, 
+  getProductPrices, 
+  getShopifyCollections, 
+  getIsacordNumbers, 
+  getAmannNumbers,
+  getProductDataLPC 
+} from "../dataFetchers";
+
+export const loader = async () => {  
+  try {
+    const [
+      leatherColors, 
+      stitchingThreadColors, 
+      embroideryThreadColors, 
+      colorTags, 
+      fonts, 
+      shapes, 
+      styles, 
+      productPrices, 
+      shopifyCollections, 
+      isacordNumbers, 
+      amannNumbers,
+      productDataLPC,
+    ] = await Promise.all([
+      getLeatherColors(),
+      getStitchingThreadColors(),
+      getEmbroideryThreadColors(),
+      getColorTags(),
+      getFonts(),
+      getShapes(),
+      getStyles(),
+      getProductPrices(),
+      getShopifyCollections(),
+      getIsacordNumbers(),
+      getAmannNumbers(),
+      getProductDataLPC()
+    ]);
+
+    return {
+      shopifyCollections,
+      leatherColors,
+      stitchingThreadColors,
+      embroideryThreadColors,
+      amannNumbers,
+      isacordNumbers,
+      colorTags,
+      shapes,
+      styles,
+      fonts,
+      productPrices,
+      productDataLPC,
+      error: null
+    };
+  } catch (error) {
+    console.error("Loader Error:", error);
+    return new Response(
+      JSON.stringify({
+        shopifyCollections: [],
+        leatherColors: [],
+        stitchingThreadColors: [],
+        embroideryThreadColors: [],
+        amannNumbers: [],
+        isacordNumbers: [],
+        colorTags: [],
+        shapes: [],
+        styles: [],
+        fonts: [],
+        productPrices: [],
+        productDataLPC: [],
+        error: error.message
+      }), 
+      { status: 500,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+       }
+    );
+  }
+};
