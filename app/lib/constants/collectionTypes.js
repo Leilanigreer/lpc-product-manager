f8ad2c57228555f5f98eb46c9a19377c5c4740db
Leilanigreer <leilani@lpcgolf.com>
2024-12-24 19:18:54 -0800

    migrated to a folder for organization

---
+++ app/lib/constants/collectionTypes.js
@@ -0,0 +1,17 @@
+// app/lib/constants/constants.js
+
+export const COLLECTION_TYPES = {
+  QUILTED: 'Quilted',
+  ARGYLE: 'Argyle',
+  CLASSIC: 'Classic',
+  QCLASSIC: 'QClassic',
+  ANIMAL: 'Animal'
+};
+
+export const COLLECTION_HANDLE_MAP = {
+  'quilted': COLLECTION_TYPES.QUILTED,
+  'argyle': COLLECTION_TYPES.ARGYLE,
+  'classic': COLLECTION_TYPES.CLASSIC,
+  'classic-quilted': COLLECTION_TYPES.QCLASSIC,
+  'animal-print': COLLECTION_TYPES.ANIMAL
+};
